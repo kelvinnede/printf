b34836db68d83196bd73c1d40061cf039fd83cfe
Kelvin Nede <kelvinnede@gmail.com>
2023-11-12 13:16:34 -0800

    printf.c

--- printf.c
+++ printf.c
@@ -1,19 +1,16 @@
-#include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
-#include <stdio.h>
+
 /**
  * _printf - Custom printf function
  * @format: Format string
  *
  * Return: Number of characters printed (excluding the null byte)
  */
-
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int count = 0, num_length;
-	char c, *str = NULL, num_str[12];
+	int count = 0;
 
 	va_start(args, format);
 
@@ -21,25 +18,32 @@ int _printf(const char *format, ...)
 	{
 		if (*format == '%' && (*(format + 1) == 'c' || *(format + 1) == 's'))
 		{
+			/* Handle %c and %s cases */
 			if (*(format + 1) == 'c')
+			{
+				char c = (char)va_arg(args, int);
+
 				count += write(1, &c, 1);
-			else
-				str = va_arg(args, char*), count += write(1, str, 0);
-			format += 2;
-		}
-		else if (*format == '%' && (*(format + 1) == 'd' || *(format + 1) == 'i'))
-		{
-			num_length = snprintf(num_str, sizeof(num_str), "%d", va_arg(args, int));
-			count += write(1, num_str, num_length);
+			}
+			else if (*(format + 1) == 's')
+			{
+				count += write(1, va_arg(args, char*), 1);
+			}
+
 			format += 2;
 		}
 		else if (*format == '%' && *(format + 1) == '%')
 		{
+			/* Handle %% case */
 			count += write(1, "%", 1);
 			format += 2;
 		}
 		else
-			count += write(1, format++, 1);
+		{
+			/* Regular character, just print it */
+			count += write(1, format, 1);
+			format++;
+		}
 	}
 
 	va_end(args);
