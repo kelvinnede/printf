4c64c70efe5a54e5426a884c780a2fb048931294
Kelvin Nede <kelvinnede@gmail.com>
2023-11-12 13:42:49 -0800

    printf.c

--- printf.c
+++ printf.c
@@ -1,8 +1,7 @@
 #include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
-#include <string.h>
-
+#include <stdio.h>
 /**
  * _printf - Custom printf function
  * @format: Format string
@@ -12,7 +11,8 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int count = 0;
+	int count = 0, num_length;
+	char c, *str = NULL, num_str[12];
 
 	va_start(args, format);
 
@@ -20,32 +20,25 @@ int _printf(const char *format, ...)
 	{
 		if (*format == '%' && (*(format + 1) == 'c' || *(format + 1) == 's'))
 		{
-			/* Handle %c and %s cases */
 			if (*(format + 1) == 'c')
-			{
-				char c = (char)va_arg(args, int);
-
 				count += write(1, &c, 1);
-			}
-			else if (*(format + 1) == 's')
-			{
-				count += write(1, va_arg(args, char*), 1);
-			}
-
+			else
+				str = va_arg(args, char*), count += write(1, str, 0);
+			format += 2;
+		}
+		else if (*format == '%' && (*(format + 1) == 'd' || *(format + 1) == 'i'))
+		{
+			num_length = snprintf(num_str, sizeof(num_str), "%d", va_arg(args, int));
+			count += write(1, num_str, num_length);
 			format += 2;
 		}
 		else if (*format == '%' && *(format + 1) == '%')
 		{
-			/* Handle %% case */
 			count += write(1, "%", 1);
 			format += 2;
 		}
 		else
-		{
-			/* Regular character, just print it */
-			count += write(1, format, 1);
-			format++;
-		}
+			count += write(1, format++, 1);
 	}
 
 	va_end(args);
