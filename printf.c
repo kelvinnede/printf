0510cd91e3f9293b70204314ab4e2cb4b0916307
Kelvin Nede <kelvinnede@gmail.com>
2023-11-13 09:22:02 -0800

    printf.c

--- printf.c
+++ printf.c
@@ -14,24 +14,24 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int count = 0, num_length;
-	char num_str[12];
+	char c, *str = NULL, num_str[12];
 
 	va_start(args, format);
 
 	while (*format)
 	{
-		if (*format == '%' && (*(format + 1) == 'd' || *(format + 1) == 'i'))
+		if (*format == '%' && (*(format + 1) == 'c' || *(format + 1) == 's'))
 		{
-			num_length = snprintf(num_str, sizeof(num_str), "%d", va_arg(args, int));
-
-			count += write(1, num_str, num_length);
+			if (*(format + 1) == 'c')
+				count += write(1, &c, 1);
+			else
+				str = va_arg(args, char *), count += write(1, str, 0);
 			format += 2;
 		}
-		else if (*format == '%' && *(format + 1) == 'b')
+		else if (*format == '%' && (*(format + 1) == 'd' || *(format + 1) == 'i'))
 		{
-			unsigned int num = va_arg(args, unsigned int);
-
-			print_binary(num);
+			num_length = snprintf(num_str, sizeof(num_str), "%d", va_arg(args, int));
+			count += write(1, num_str, num_length);
 			format += 2;
 		}
 		else if (*format == '%' && *(format + 1) == '%')
@@ -40,9 +40,7 @@ int _printf(const char *format, ...)
 			format += 2;
 		}
 		else
-		{
 			count += write(1, format++, 1);
-		}
 	}
 
 	va_end(args);
