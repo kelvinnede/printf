2334ab02c506df46e6d791b7d98c75a875a13fcf
Kelvin Nede <kelvinnede@gmail.com>
2023-11-12 23:46:48 -0800

    printf.c

--- printf.c
+++ printf.c
@@ -35,52 +35,45 @@ int _puts(char *str)
 }
 
 /**
- * _printf - Custom printf function
- * @format: Format string
+ * _printf - Custom printf function.
+ * @format: Format string containing format specifiers.
  *
- * Return: Number of characters printed (excluding null byte)
+ * Return: The number of characters printed.
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int count = 0;
+	int count = 0, num_length;
+	char c, *str = NULL, num_str[12];
 
 	va_start(args, format);
 
 	while (*format)
 	{
-		if (*format == '%' && (
-			*(format + 1) == 'c' ||
-			*(format + 1) == 's' ||
-			*(format + 1) == '%'
-		))
+		if (*format == '%' && (*(format + 1) == 'c' || *(format + 1) == 's'))
 		{
-			format++; /* Move to the conversion specifier */
-			switch (*format)
-			{
-				case 'c':
-					count += _putchar(va_arg(args, int));
-					break;
-				case 's':
-					count += _puts(va_arg(args, char *));
-					break;
-				case '%':
-					count += _putchar('%');
-					break;
-				default:
-					count += _putchar('%');
-					count += _putchar(*format);
-			}
+			if (*(format + 1) == 'c')
+				count += write(1, &c, 1);
+			else
+				str = va_arg(args, char *), count += write(1, str, 0);
+			format += 2;
+		}
+		else if (*format == '%' && (*(format + 1) == 'd' || *(format + 1) == 'i'))
+		{
+			num_length = snprintf(num_str, sizeof(num_str), "%d", va_arg(args, int));
+			count += write(1, num_str, num_length);
+			format += 2;
+		}
+		else if (*format == '%' && *(format + 1) == '%')
+		{
+			count += write(1, "%", 1);
+			format += 2;
 		}
 		else
-		{
-			count += _putchar(*format);
-		}
-		format++;
+			count += write(1, format++, 1);
 	}
 
 	va_end(args);
 
 	return (count);
 }
-
