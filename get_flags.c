982e9023b7aeb94e4f3ecb81271cf523adc3cb0f
chidopraise <abrahamlivinus@gmail.com>
2023-11-14 10:29:16 -0800

    get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
- * Return: Flags:
+ * get_flags - This function calculates active flags
+ * @format: The format parameter gets the formatted string
+ * in which to print the arguments
+ * @i: This argument takes a parameter to be formatted.
+ * Return: Returns a Flags.
  */
 int get_flags(const char *format, int *i)
 {
